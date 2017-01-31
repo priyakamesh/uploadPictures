228460b7da261f8ce70f33f94638155b5d602ef0
krishnapriya <krishnapriyakamesh@gmail.com>
2017-01-31 12:10:27 -0600

    uploading pics done

--- app/app.js
+++ app/app.js
@@ -3,6 +3,13 @@ console.log("lets start")
 const app = angular.module("uploadApp", ['ngRoute'])
 .config (($routeProvider,$locationProvider)=>{
   $locationProvider.hashPrefix('')
+   firebase.initializeApp({
+    apiKey: "AIzaSyDvTGEJoO53RaYQ6muv0APGUFyKesNxE_4",
+    authDomain: "upload-pictures.firebaseapp.com",
+    databaseURL: "https://upload-pictures.firebaseio.com",
+    storageBucket: "upload-pictures.appspot.com",
+    messagingSenderId: "977932051303"
+  });
   $routeProvider
     .when ("/",{
       controller: "HomeCtrl",
--- app/controller/home.js
+++ app/controller/home.js
@@ -1,3 +1,26 @@
 app.controller("HomeCtrl", function(){
   console.log("home ctrl")
+  //root reference to firebase
+  let storageRef = firebase.storage().ref();
+
+  let inputElement = document.getElementById("fileInput");
+  inputElement.addEventListener("change", handleFiles, false)
+  function handleFiles() {
+    var fileList = this.files; /* now you can work with the file list */
+    console.log("filelist[0]", fileList[0])
+    storageRef.child(fileList[0].name).put(fileList[0])
+      .then(function(snapshot) {
+        console.log('Uploaded a blob or file!');
+
+
+    storageRef.child(fileList[0].name).getDownloadURL()
+    .then((url)=>{
+      var img =document.getElementById("myImg")
+      img.src = url;
+    })
+    .catch((error)=>{
+      alert("error")
+    })
+    });
+  }
 })
